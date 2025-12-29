413366add6e594718cd1325e36a632d21a445fe1
suraj kannujiya <82014093+Suraj-1233@users.noreply.github.com>
2025-12-30 00:15:16 +0530

    Update API base URL for production environment

--- frontend/src/app/constants/api-endpoints.ts
+++ frontend/src/app/constants/api-endpoints.ts
@@ -1,5 +1,5 @@
 export const API_ENDPOINTS = {
-   BASE_URL: 'https://www.servicesarkari.com/api',
+   BASE_URL: 'https://backend-service-rbor.onrender.com/api',
   //BASE_URL: 'http://localhost:8080',
     RECORDS: {
       GET_ALL: '/records',
